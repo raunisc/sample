4bb97ac2dea25b6f8c2d69e5ecbe21be47dab3a3
rauni <77559846+raunisc@users.noreply.github.com>
2022-02-03 11:49:30 -0300

    sample

---
+++ js.js
@@ -0,0 +1,6 @@
+function clique(){
+    alert("Parabens voce clicou mesmo!!")
+}
+function erro(){
+    alert("Entao, deixa eu te explicar.... voce clicou errado, tente novamente!")
+}
--- sample/js.js
+++
@@ -1,6 +0,0 @@
-function clique(){
-    alert("Parabens voce clicou mesmo!!")
-}
-function erro(){
-    alert("Entao, deixa eu te explicar.... voce clicou errado, tente novamente!")
-}
